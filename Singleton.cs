55512372f9affd0c6678ed2d7eebf97cb38e5dcd
ERROR996724 <794797908@qq.com>
2022-07-17 09:25:32 +0800

    Create Singleton.cs
    A Singleton Template

---
+++ Singleton.cs
@@ -0,0 +1,30 @@
+public class Singleton<T> : MonoBehaviour where T:Singleton<T>
+{
+    private static T instance;
+
+    public static T Instance
+    {
+        get { return instance; }
+    }
+
+    protected virtual void Awake()
+    {
+        if (instance != null)
+            Destroy(gameObject);
+        else
+            instance = (T)this;
+    }
+    
+    public static bool isInitialized
+    {
+        get { return instance != null; }
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if (instance == this)
+        {
+            instance = null;
+        }
+    }
+}
